045a21e893c580c2fd7827803c18505554289249
Henrique <henriquemiranda47@gmail.com>
2020-08-20 10:26:23 -0300

    Imported tensorfow

--- Main.py
+++ Main.py
@@ -1,10 +1,20 @@
+from sklearn import model_selection, preprocessing, linear_model, naive_bayes, metrics, svm
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn import decomposition, ensemble
+import warnings
+with warnings.catch_warnings():  
+    warnings.filterwarnings("ignore",category=FutureWarning)
+    import pandas, xgboost, numpy, textblob, string
+    from keras.preprocessing import text, sequence
+    from keras import layers, models, optimizers
+
 
 
 
 class Program:
-    
-    def main():
 
+    def main():
+        
         print("Finalizando.......")
 
 
