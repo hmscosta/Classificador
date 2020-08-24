3aa6c7e598bca63fb8f4c50f4aaef13654b2047c
Henrique <henriquemiranda47@gmail.com>
2020-08-24 10:01:03 -0300

    Added news dataset for tests

--- Classificadores/Filmes.py
+++ Classificadores/Filmes.py
@@ -1,9 +1,14 @@
-from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+from sklearn.datasets import fetch_20newsgroups
 class Filmes:
 
     nome = "classificador_de_filmes"
 
 
     def classificador(self):
-            print("%s trabalhando" % self.nome)
+        print("%s trabalhando" % self.nome)
+        categories = ['alt.atheism', 'soc.religion.christian','comp.graphics', 'sci.med']
+        twenty_train = fetch_20newsgroups(subset='train',categories=categories, shuffle=True, random_state=42)
+        print(twenty_train.target_names)
+        
            
