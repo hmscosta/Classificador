2c806977fc40c06b188d4168f9f01a36b06439a5
Henrique <henriquemiranda47@gmail.com>
2020-08-26 10:41:33 -0300

    Imported tutorial dataset and json library

--- Classificadores/Tutorial.py
+++ Classificadores/Tutorial.py
@@ -1,14 +1,21 @@
-import pandas as pd
-from sklearn import datasets
+import json
 
 class Tutorial:
 
     nome = "classificador_tutorial"
-
+    file_path = "/home/henrique/Documents/Desenvolvimento/Python/Datasets/Books_small.json"
+    reviews = []
 
     def classificador(self):
         print("%s trabalhando" % self.nome)
-        iris = datasets.load_iris()
-        digits = datasets.load_digits()
-        print(digits.data)
+        arquivo = open(self.file_path,"r")
+        for line in arquivo:
+            review = json.loads(line)
+            #print(review['reviewText'])
+            #print(review['overall'])
+            self.reviews.append((review['reviewText'], review['overall']))
+        arquivo.close()
+        print(self.reviews[0])
+
+
            
