d91601ca13575202b37b22497659d3ceb0976e53
Henrique <henriquemiranda47@gmail.com>
2020-08-27 11:23:49 -0300

    Added bag of words

--- Classificadores/Tutorial.py
+++ Classificadores/Tutorial.py
@@ -1,5 +1,7 @@
 import json
 from ClassesDeAuxilio.Review import Review
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
 class Tutorial:
 
     nome = "classificador_tutorial"
@@ -13,8 +15,23 @@ class Tutorial:
             review = json.loads(line)
             self.reviews.append(Review(review['reviewText'], review['overall']))
         arquivo.close()
-        print(self.reviews[0].score)
-        print(self.reviews[0].sentiment)
+        
+        #Separando os dados em teste e treinamento
+        training, test = train_test_split(self.reviews, test_size=0.33, random_state=42)
+        #Separando score e text em duas arrays
+        train_x = [x.text for x in training]
+        train_y = [x.sentiment for x in training]
+        test_x = [x.text for x in test]
+        test_y = [x.sentiment for x in test]
+        
 
+        
+        #Rodando o algoritimo de bag of words
+        vectorizer = CountVectorizer()
+        train_x_vectors = vectorizer.fit_transform(train_x) #Cria os vetores em formato numerico
+        print(train_x[0])    #Primeira linha de texto
+        print(train_x_vectors[0].toarray()) #Vetor numerico que representa a primeira linha de texto
 
+        #print(vectorizer.get_feature_names())
+        
            
