150a96a01d5640fe92808abe2ac1384d0a734250
Henrique <henriquemiranda47@gmail.com>
2020-08-31 11:16:08 -0300

    Added bag of words on jobs

--- Classificadores/ClassificadorVagas.py
+++ Classificadores/ClassificadorVagas.py
@@ -16,6 +16,23 @@ class ClassificadorVagas:
             for cont in range(len(review)):
                 self.vagas.append(Vaga(review[cont]['descricaoVaga'], review[cont]['tagVaga']))
         arquivo.close()
+        ##print(self.vagas[0].descricao)
+        ##print(self.vagas[0].tagVaga)
+        #Separando os dados em teste e treinamento
+        training, test = train_test_split(self.vagas, test_size=0.33, random_state=42)
+        print(len(training))
+        print(len(test))
+        #Separando descricao e tagVaga em duas arrays
+        train_x = [x.descricao for x in training]
+        train_y = [x.tagVaga for x in training]
+        test_x = [x.descricao for x in test]
+        test_y = [x.tagVaga for x in test]
+       
 
-        print(self.vagas[0].descricao)
-        print(self.vagas[0].tagVaga)
+        #Rodando o algoritimo de bag of words
+        vectorizer = CountVectorizer()
+        train_x_vectors = vectorizer.fit_transform(train_x) #Cria os vetores em formato numerico
+        ##print(train_x[0])    #Primeira linha de texto
+        ##rint(train_x_vectors[0].toarray()) #Vetor numerico que representa a primeira linha de texto
+
+        print(vectorizer.get_feature_names())
