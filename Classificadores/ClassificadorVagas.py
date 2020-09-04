ddca77100dd41686cfebb14ad49cb0a52eb0439d
Henrique <henriquemiranda47@gmail.com>
2020-09-04 10:01:54 -0300

    First version of the text classifier for jobs

--- Classificadores/ClassificadorVagas.py
+++ Classificadores/ClassificadorVagas.py
@@ -1,15 +1,23 @@
 import json
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from ClassesDeAuxilio.Vaga import Vaga
+from sklearn import tree
+from sklearn import svm
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import f1_score
+from sklearn.model_selection import GridSearchCV
 
 class ClassificadorVagas:
 
     nome = "classificador_de_vagas"
     vagas = []
+    teste = []
+    
     def classificador(self):
         print("%s trabalhando" % self.nome)
-        file_path = "/home/henrique/Documents/Desenvolvimento/Python/Aprendizado/PythonScrap/texto/vagas.json"
+        file_path = "/home/henrique/Documents/Desenvolvimento/Python/Datasets/vagas.json"
         arquivo = open(file_path,"r")
         for line in arquivo:
             review = json.loads(line)
@@ -20,8 +28,10 @@ class ClassificadorVagas:
         ##print(self.vagas[0].tagVaga)
         #Separando os dados em teste e treinamento
         training, test = train_test_split(self.vagas, test_size=0.33, random_state=42)
+        
+        
         print(len(training))
-        print(len(test))
+        ##print(len(test))
         #Separando descricao e tagVaga em duas arrays
         train_x = [x.descricao for x in training]
         train_y = [x.tagVaga for x in training]
@@ -30,9 +40,22 @@ class ClassificadorVagas:
        
 
         #Rodando o algoritimo de bag of words
-        vectorizer = CountVectorizer()
+        vectorizer = TfidfVectorizer()
         train_x_vectors = vectorizer.fit_transform(train_x) #Cria os vetores em formato numerico
-        ##print(train_x[0])    #Primeira linha de texto
-        ##rint(train_x_vectors[0].toarray()) #Vetor numerico que representa a primeira linha de texto
-        ##print(vectorizer.get_feature_names())
-        #print(train_y)
+        test_x_vectors = vectorizer.transform(test_x) #Cria os vetores em formato numerico
+        #Tunando os algoritimos
+        parameters = {'kernel':('linear', 'poly', 'rbf'),
+                     'C': (1, 2, 4, 8, 10, 16, 32, 64)}
+        #svc = svm.SVC()
+        #clf_svm_tunned = GridSearchCV(svc, parameters, cv=5, verbose=10)
+        #clf_svm_tunned.fit(train_x_vectors, train_y)
+        print()
+        print()
+        #Usando o algoritimo de svm
+        clf_svm = svm.SVC(kernel = 'linear')
+        clf_svm = clf_svm.fit(train_x_vectors, train_y)
+        
+        print(test_x[0])
+        print(test_y[0])
+        print("--------------------------")
+        print(clf_svm.predict(test_x_vectors[0]))
